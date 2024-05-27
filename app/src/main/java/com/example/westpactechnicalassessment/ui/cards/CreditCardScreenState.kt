0bd78c3403e65311fc4804a951eb20dc3ea7f03d
George Zhang <george.zhang@smartpay.co.nz>
2024-05-27 15:02:22 +1200

    minor update that update format and make sealed interface to sealed class

--- app/src/main/java/com/example/westpactechnicalassessment/ui/cards/CreditCardScreenState.kt
+++ app/src/main/java/com/example/westpactechnicalassessment/ui/cards/CreditCardScreenState.kt
@@ -2,9 +2,14 @@ package com.example.westpactechnicalassessment.ui.cards
 
 import com.example.westpactechnicalassessment.domain.card.model.CreditCardInfo
 
-sealed interface CreditCardScreenState {
-    data object Initial : CreditCardScreenState
-    data object Loading : CreditCardScreenState
-    data class ShowCards (val creditCards : List<CreditCardInfo>, val isLoading: Boolean = false, val canLoadMore: Boolean = true) : CreditCardScreenState
-    data object FailedToLoad : CreditCardScreenState
+sealed class CreditCardScreenState {
+    data object Initial : CreditCardScreenState()
+    data object Loading : CreditCardScreenState()
+    data class ShowCards(
+        val creditCards: List<CreditCardInfo>,
+        val isLoading: Boolean = false,
+        val canLoadMore: Boolean = true
+    ) : CreditCardScreenState()
+
+    data object FailedToLoad : CreditCardScreenState()
 }
